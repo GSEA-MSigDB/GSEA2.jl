8938286d6cc730077097a5ddf4563039da1a2829
KwatME <kwat.me@icloud.com>
2021-10-15 12:19:19 -0600

    Port from python

--- src/GSEA.jl
+++ src/GSEA.jl
@@ -1,16 +1,6 @@
 module GSEA
 
-include("_get_probability_and_cumulative_probability.jl")
-
-include("make_benchmark.jl")
-
-include("plot_mountain.jl")
-
-include("score_set.jl")
-
-include("score_set_new.jl")
-
-include("try_method.jl")
+using Kwat
 
 using Comonicon
 
