912ba67bb65681e8d0e5e66de4a93e984f0f97cd
KwatMDPhD <kwat.me@icloud.com>
2022-03-09 15:29:46 -0700

    Day8 8

--- src/support/error_feature_score.jl
+++ src/support/error_feature_score.jl
@@ -1,9 +1,15 @@
 function error_feature_score(fe_, sc_)
 
-    if length(fe_)
+    if length(fe_) != length(unique(fe_))
+
+        error("Features are duplicated.")
+
+    end
 
 end
 
 function error_feature_score(fe_x_sa_x_sc)
+
+    error_feature_score(fe_x_sa_x_sc[!, 1], fe_x_sa_x_sc[!, 2])
 
 end
