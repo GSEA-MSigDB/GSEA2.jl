fe06d8855501ff99855af52f39013ba1fc774e84
KwatMDPhD <kwat.me@icloud.com>
2022-11-10 21:09:59 -0700

    Next is _filter_set.jl

--- src/_compute_statistic.jl
+++ src/_compute_statistic.jl
@@ -1,29 +1,37 @@
-function _compute_statistic(se_en, se_ra__, ou)
+function _compute_statistic(se_en, se_ra__, di)
 
+    #
     se_ = collect(keys(se_en))
 
     en_ = collect(values(se_en))
 
-    mkpath(ou)
+    #
+    mkpath(di)
 
+    #
     if isempty(se_ra__)
 
+        #
         gl_ = gla_ = fill(NaN, length(se_))
 
     else
 
-        ra__ = collect.(values.(se_ra__))
+        #
+        ra__ = [collect(values(se_ra_)) for se_ra_ in se_ra__]
 
-        gl_, gla_ = OnePiece.significance.get_p_value_and_adjust(en_, vcat(ra__...))
+        #
+        gl_, gla_ = OnePiece.Significance.get_p_value_and_adjust(en_, vcat(ra__...))
 
+        #
         se_x_ra_x_en = DataFrame("Set" => se_)
 
-        insertcols!(se_x_ra_x_en, (string(id) => ra_ for (id, ra_) in enumerate(ra__))...)
+        insertcols!(se_x_ra_x_en, (id => ra_ for (id, ra_) in enumerate(ra__))...)
 
-        OnePiece.table.write(joinpath(ou, "set_x_random_x_enrichment.tsv"), se_x_ra_x_en)
+        OnePiece.Table.write(joinpath(di, "set_x_random_x_enrichment.tsv"), se_x_ra_x_en)
 
     end
 
+    #
     se_x_st_x_nu = sort(
         DataFrame(
             "Set" => se_,
@@ -34,7 +42,7 @@ function _compute_statistic(se_en, se_ra__, ou)
         "Enrichment",
     )
 
-    OnePiece.table.write(joinpath(ou, "set_x_statistic_x_number.tsv"), se_x_st_x_nu)
+    OnePiece.Table.write(joinpath(di, "set_x_statistic_x_number.tsv"), se_x_st_x_nu)
 
     se_x_st_x_nu
 
