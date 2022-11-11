3c0734e0b44b135f586f0ffb793afd0bd5f4119a
KwatMDPhD <kwat.me@icloud.com>
2022-11-11 08:44:56 -0700

    Next is _plot_mountain.jl 2

--- src/_plot_mountain.jl
+++ src/_plot_mountain.jl
@@ -1,15 +1,19 @@
-function _plot_mountain(se_x_st_x_nu, n_ex, pl_, al, fe_, sc_, se_fe_, sy_ar, ou)
+function _plot_mountain(se_x_st_x_nu, n_ex, pl_, al, fe_, sc_, se_fe_, sy_ar, di)
 
+    #
     n_se = size(se_x_st_x_nu, 1)
 
-    n_ex = minimum([n_ex, n_se])
+    #
+    n_ex = min(n_ex, n_se)
 
     co_ = [1, 2]
 
+    #
     for ro in 1:n_ex
 
         se, en = se_x_st_x_nu[ro, co_]
 
+        #
         if en <= 0 && !(se in pl_)
 
             push!(pl_, se)
@@ -18,6 +22,7 @@ function _plot_mountain(se_x_st_x_nu, n_ex, pl_, al, fe_, sc_, se_fe_, sy_ar, ou
 
     end
 
+    #
     for ro in n_se:-1:(n_se - n_ex + 1)
 
         se, en = se_x_st_x_nu[ro, co_]
@@ -30,10 +35,10 @@ function _plot_mountain(se_x_st_x_nu, n_ex, pl_, al, fe_, sc_, se_fe_, sy_ar, ou
 
     end
 
-    di = joinpath(ou, "plot")
+    #
+    pl = mkpath(joinpath(di, "plot"))
 
-    mkpath(di)
-
+    #
     pop!(sy_ar, :n_jo)
 
     if al == "cidac"
@@ -46,6 +51,7 @@ function _plot_mountain(se_x_st_x_nu, n_ex, pl_, al, fe_, sc_, se_fe_, sy_ar, ou
 
     end
 
+    #
     for se in pl_
 
         fu(
@@ -54,7 +60,7 @@ function _plot_mountain(se_x_st_x_nu, n_ex, pl_, al, fe_, sc_, se_fe_, sy_ar, ou
             se_fe_[se];
             title_text = se,
             fe = "Gene",
-            ou = joinpath(di, "$(OnePiece.path.clean(se, pr = false)).html"),
+            di = joinpath(pl, "$(OnePiece.Path.clean(se)).html"),
             sy_ar...,
         )
 
