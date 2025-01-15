64bafc365d4b1313bf9e2ff1a1baa9cbe653f4ee
KwatMDPhD <kwat.me@icloud.com>
2025-01-14 23:39:09 -0700

    GSEA

--- src/GSEA.jl
+++ src/GSEA.jl
@@ -617,6 +617,788 @@ function enrich(al, fe_, sc, me___; um = 1, ex = 1)
 
 end
 
-#include("command_line.jl")
+using Comonicon: @cast, @main
+
+using ProgressMeter: @showprogress
+
+using Random: seed!, shuffle!
+
+using StatsBase: mean, sample, std
+
+using CLSGCTGMT
+
+function plot(ou, fe_, sc, al, se_, me___, ns, sa_, en; ex = 1.0, up = 4)
+
+    Omics.Plot.plot_heat_map(
+        joinpath(ou, "set_x_sample_x_enrichment.html"),
+        en;
+        ro_ = se_,
+        co_ = sa_,
+        la = Dict(
+            "title" => Dict("text" => "Enrichment using $(string(al)[6:(end - 2)])"),
+            "yaxis" =>
+                Dict("title" => Dict("text" => Omics.Strin.coun(lastindex(se_), "Set"))),
+            "xaxis" =>
+                Dict("title" => Dict("text" => Omics.Strin.coun(lastindex(sa_), ns))),
+        ),
+    )
+
+    ge_ = map(!isnan, en)
+
+    gs_ = BitVector(undef, lastindex(fe_))
+
+    for id_ in CartesianIndices(en)[ge_][Omics.Extreme.ge(en[ge_], up)]
+
+        ie, ia = Tuple(id_)
+
+        sc_ = sc[:, ia]
+
+        map!(!isnan, gs_, sc_)
+
+        so_ = sc_[gs_]
+
+        id_ = sortperm(so_; rev = true)
+
+        ti = "$(sa_[ia]) Enriching $(se_[ie])"
+
+        plot(
+            joinpath(ou, "$ti.html"),
+            al,
+            fe_[gs_][id_],
+            so_[id_],
+            me___[ie];
+            ex,
+            la = Dict("title" => Dict("text" => ti)),
+        )
+
+    end
+
+end
+
+function _standardize_clamp!(ma, di, st)
+
+    if isone(di)
+
+        ea = eachrow
+
+    elseif di == 2
+
+        ea = eachcol
+
+    end
+
+    foreach(Omics.Normalization.normalize_with_0!, ea(ma))
+
+    clamp!(ma, -st, st)
+
+end
+
+function _read_set(js, fe_, mi, ma, fr)
+
+    se_me_ = Omics.Dic.rea(js)
+
+    se_ = collect(keys(se_me_))
+
+    me___ = collect(values(se_me_))
+
+    ke_ = BitVector(undef, lastindex(se_))
+
+    for id in eachindex(me___)
+
+        me_ = me___[id]
+
+        ub = lastindex(me_)
+
+        intersect!(me_, fe_)
+
+        ua = lastindex(me_)
+
+        ke_[id] = mi <= ua <= ma && fr <= ua / ub
+
+    end
+
+    se_[ke_], me___[ke_]
+
+end
+
+function _set_algorithm(al)
+
+    if al == "ks"
+
+        KS()
+
+    elseif al == "ksa"
+
+        KSa()
+
+    elseif al == "kliom"
+
+        KLioM()
+
+    elseif al == "kliop"
+
+        KLioP()
+
+    elseif al == "kli"
+
+        KLi()
+
+    elseif al == "kli1"
+
+        KLi1()
+
+    end
+
+end
+
+"""
+Convert `.cls` and `.gct` to two `.tsv`s.
+
+# Args
+
+  - `target_x_sample_x_number_tsv`: Output target `.tsv`.
+  - `feature_x_sample_x_score_tsv`: Output feature `.tsv`.
+  - `cls`: Input `.cls`.
+  - `gct`: Input `.gct`.
+"""
+@cast function convert_cls_gct(
+    target_x_sample_x_number_tsv,
+    feature_x_sample_x_score_tsv,
+    cls,
+    gct,
+)
+
+    cl = CLSGCTGMT.read_cls(cls)
+
+    gc = CLSGCTGMT.read_gct(gct)
+
+    sa_ = names(gc)[2:end]
+
+    nu = Matrix(cl[:, 2:end])
+
+    map!(nm -> nm - 1.0, nu, nu)
+
+    Omics.Table.writ(target_x_sample_x_number_tsv, "Target", cl[:, 1], sa_, nu)
+
+    Omics.Table.writ(
+        feature_x_sample_x_score_tsv,
+        "Feature",
+        gc[:, 1],
+        sa_,
+        Matrix(gc[:, 2:end]),
+    )
+
+    target_x_sample_x_number_tsv, feature_x_sample_x_score_tsv
+
+end
+
+"""
+Convert (merging) `.gmt`s to `.json`.
+
+# Args
+
+  - `set_features_json`: Output `.json`.
+  - `gmt_`: Input `.gmt`s.
+"""
+@cast function convert_gmt(set_features_json, gmt_...)
+
+    Omics.Dic.writ(set_features_json, reduce(merge!, gmt_))
+
+end
+
+"""
+Run data-rank (single-sample) GSEA.
+
+# Args
+
+  - `output_directory`:
+  - `feature_x_sample_x_score_tsv`:
+  - `set_features_json`:
+
+# Options
+
+  - `--normalization-dimension`: = 0. 0 (not normalizing) | 1 | 2.
+  - `--normalization-standard-deviation`: = 4.
+  - `--algorithm`: = "ks". "ks" | "ksa" | "kliom" | "kliop" | "kli" | "kli1".
+  - `--exponent`: = 1.0.
+  - `--post-skip-minimum-set-size`: = 1.
+  - `--minimum-set-size`: = 15.
+  - `--maximum-set-size`: = 500.
+  - `--minimum-set-fraction`: = 0.0.
+
+# Flags
+
+  - `--skip-0`: = false. Set this to true for single-cell or other sparse data.
+"""
+@cast function data_rank(
+    output_directory,
+    feature_x_sample_x_score_tsv,
+    set_features_json;
+    skip_0::Bool = false,
+    normalization_dimension::Int = 0,
+    normalization_standard_deviation::Float64 = 4.0,
+    algorithm = "ks",
+    exponent::Float64 = 1.0,
+    post_skip_minimum_set_size::Int = 1,
+    minimum_set_size::Int = 15,
+    maximum_set_size::Int = 500,
+    minimum_set_fraction::Float64 = 0.0,
+)
+
+    ta = Omics.Table.rea(feature_x_sample_x_score_tsv)
+
+    fe_ = ta[!, 1]
+
+    sa_ = names(ta)[2:end]
+
+    sc = Matrix(ta[!, 2:end])
+
+    if skip_0
+
+        replace!(sc, 0.0 => NaN)
+
+    end
+
+    if !iszero(normalization_dimension)
+
+        _standardize_clamp!(sc, normalization_dimension, normalization_standard_deviation)
+
+    end
+
+    al = _set_algorithm(algorithm)
+
+    se_, me___ = _read_set(
+        set_features_json,
+        fe_,
+        minimum_set_size,
+        maximum_set_size,
+        minimum_set_fraction,
+    )
+
+    en = enrich(al, fe_, sc, me___; um = post_skip_minimum_set_size, ex = exponent)
+
+    Omics.Table.writ(
+        joinpath(output_directory, "set_x_sample_x_enrichment.tsv"),
+        "Set",
+        se_,
+        sa_,
+        en,
+    )
+
+    plot(output_directory, fe_, sc, al, se_, me___, "Sample", sa_, en; ex = exponent)
+
+end
+
+function _separate(nu_)
+
+    ie_ = findall(<(0.0), nu_)
+
+    ip_ = findall(>=(0.0), nu_)
+
+    nu_[ie_], nu_[ip_]
+
+end
+
+function _normalize_enrichment(nu, mn, mp)
+
+    nu / nu < 0.0 ? -mn : mp
+
+end
+
+function _normalize_enrichment(nu, mn, mp, sn, sp)
+
+    if nu < 0.0
+
+        me = mn
+
+        st = -sn
+
+    else
+
+        me = mp
+
+        st = sp
+
+    end
+
+    1.0 + (nu - me) / 3.0 * st
+
+end
+
+function _normalize_enrichment!(::Union{KS, KSa}, en_, ra)
+
+    er_ = Vector{Float64}(undef, lastindex(en_))
+
+    for id in eachindex(en_)
+
+        en = en_[id]
+
+        ra_ = ra[id, :]
+
+        ne_, po_ = _separate(ra_)
+
+        mn = mean(ne_)
+
+        mp = mean(po_)
+
+        er_[id] = _normalize_enrichment(en, mn, mp)
+
+        map!(ra -> _normalize_enrichment(ra, mn, mp), ra_, ra_)
+
+    end
+
+    er_
+
+end
+
+function _normalize_enrichment!(::Union{KLi1, KLi, KLioM, KLioP}, en_, ra)
+
+    er_ = Vector{Float64}(undef, lastindex(en_))
+
+    for id in eachindex(en_)
+
+        en = en_[id]
+
+        ra_ = ra[id, :]
+
+        ne_, po_ = _separate(ra_)
+
+        mn = mean(ne_)
+
+        mp = mean(po_)
+
+        sn = std(ne_)
+
+        sp = std(po_)
+
+        er_[id] = _normalize_enrichment(en, mn, mp, sn, sp)
+
+        map!(ra -> _normalize_enrichment(ra, mn, mp, sn, sp), ra_, ra_)
+
+    end
+
+    er_
+
+end
+
+function _write(ou, wr, al, fe_, sc_, ex, se_, me___, en_, ra, up, pl_, nf, ns, nl, nh)
+
+    ue = lastindex(se_)
+
+    re = fill(NaN, ue, 4)
+
+    id_ = sortperm(en_)
+
+    se_ = se_[id_]
+
+    me___ = me___[id_]
+
+    en_ = en_[id_]
+
+    ra = ra[id_, :]
+
+    if wr
+
+        Omics.Table.writ(
+            joinpath(ou, "set_x_index_x_random.tsv"),
+            "Set",
+            se_,
+            1:size(ra, 2),
+            ra,
+        )
+
+    end
+
+    er_ = _normalize_enrichment!(al, en_, ra)
+
+    id = findlast(<(0.0), en_)
+
+    ie_ = 1:id
+
+    ip_ = (id + 1):ue
+
+    np_, nq_, pp_, pq_ = Omics.Statistics.get(ra, er_, ie_, ip_)
+
+    re = stack(en_, er_, vcat(np_, pp_), vcat(nq_, pq_))
+
+    Omics.Table.writ(
+        joinpath(ou, "set_x_statistic_x_result.tsv"),
+        "Set",
+        se_,
+        ["Enrichment", "Normalized Enrichment", "P-Value", "Q-Value"],
+        re,
+    )
+
+    for id in unique!(vcat(Omics.Extreme.ge(er_, up), indexin(pl_, se_)))
+
+        if isnothing(id)
+
+            continue
+
+        end
+
+        ti = "$id $(se_[id])"
+
+        plot(
+            joinpath(ou, "$ti.html"),
+            al,
+            fe_,
+            sc_,
+            me___[id];
+            ex,
+            nf,
+            ns,
+            nl,
+            nh,
+            la = Dict("title" => Dict("text" => ti)),
+        )
+
+    end
+
+end
+
+function _permute_set(ur, se, al, fe_, sc_, me___, ex)
+
+    ra = Matrix{Float64}(undef, lastindex(me___), ur)
+
+    if !iszero(ur)
+
+        um_ = map(lastindex, me___)
+
+        seed!(se)
+
+        @showprogress for id in 1:ur
+
+            ra[:, id] =
+                enrich(al, fe_, sc_, map(um -> sample(fe_, um; replace = false), um_); ex)
+
+        end
+
+    end
+
+    ra
+
+end
+
+function _use_permutation(permutation, al, fe_, me___, ex)
+
+    ta = Omics.Table.rea(permutation)
+
+    sc = Matrix(ta[indexin(fe_, ta[!, 1]), 2:end])
+
+    ra = Matrix{Float64}(undef, lastindex(me___), size(ra, 2))
+
+    @showprogress for id in axes(sc, 2)
+
+        sc_ = sc[:, id]
+
+        id_ = sortperm(sc_; rev = true)
+
+        ra[:, id] = enrich(al, fe_[id_], sc_[id_], me___; ex)
+
+    end
+
+    ra
+
+end
+
+"""
+Run user-rank (pre-rank) GSEA.
+
+# Args
+
+  - `output_directory`:
+  - `feature_x_metric_x_score_tsv`:
+  - `set_features_json`:
+
+# Options
+
+  - `--algorithm`: = "ks". "ks" | "ksa" | "kliom" | "kliop" | "kli" | "kli1".
+  - `--exponent`: = 1.0.
+  - `--minimum-set-size`: = 15.
+  - `--maximum-set-size`: = 500.
+  - `--minimum-set-fraction`: = 0.0.
+  - `--permutation`: = "set". "set" | feature_x_index_x_random.tsv.
+  - `--number-of-permutations`: = 100.
+  - `--random-seed`: = 20150603.
+  - `--number-of-sets-to-plot`: = 4.
+  - `--more-sets-to-plot`: = "". Space-separated set names.
+  - `--feature-name`: = "Gene".
+  - `--score-name`: = "User-Defined Score".
+  - `--low-text`: = "Low".
+  - `--high-text`: = "High".
+
+# Flags
+
+  - `--write-set-x-index-x-random-tsv`: = false.
+"""
+@cast function user_rank(
+    output_directory,
+    feature_x_metric_x_score_tsv,
+    set_features_json;
+    algorithm = "ks",
+    exponent::Float64 = 1.0,
+    minimum_set_size::Int = 15,
+    maximum_set_size::Int = 500,
+    minimum_set_fraction::Float64 = 0.0,
+    permutation = "set",
+    number_of_permutations::Int = 100,
+    random_seed::Int = 20150603,
+    write_set_x_index_x_random_tsv::Bool = false,
+    number_of_sets_to_plot::Int = 4,
+    more_sets_to_plot = "",
+    feature_name = "Gene",
+    score_name = "User-Defined Score",
+    low_text = "Low",
+    high_text = "High",
+)
+
+    al = _set_algorithm(algorithm)
+
+    ta = Omics.Table.rea(feature_x_metric_x_score_tsv)
+
+    fe_ = ta[!, 1]
+
+    sc_ = ta[!, 2]
+
+    id_ = sortperm(sc_; rev = true)
+
+    fe_ = fe_[id_]
+
+    sc_ = sc_[id_]
+
+    se_, me___ = _read_set(
+        set_features_json,
+        fe_,
+        minimum_set_size,
+        maximum_set_size,
+        minimum_set_fraction,
+    )
+
+    ra = if permutation == "set"
+
+        _permute_set(number_of_permutations, random_seed, al, fe_, sc_, me___, exponent)
+
+    elseif isfile(permutation)
+
+        _use_permutation(permutation, al, fe_, me___, exponent)
+
+    end
+
+    _write(
+        output_directory,
+        write_set_x_index_x_random_tsv,
+        al,
+        fe_,
+        sc_,
+        exponent,
+        se_,
+        me___,
+        enrich(al, fe_, sc_, me___; ex = exponent),
+        ra,
+        number_of_sets_to_plot,
+        split(more_sets_to_plot),
+        feature_name,
+        score_name,
+        low_text,
+        high_text,
+    )
+
+end
+
+function _get_mean_difference(n1_, n2_)
+
+    mean(n1_) - mean(n2_)
+
+end
+
+function _get_standard_deviation(nu_, me)
+
+    max(0.2 * abs(me), std(nu_; corrected = true))
+
+end
+
+function _get_signal_to_noise_ratio(n1_, n2_)
+
+    m1 = mean(n1_)
+
+    m2 = mean(n2_)
+
+    (m1 - m2) / (_get_standard_deviation(n1_, m1) + _get_standard_deviation(n2_, m2))
+
+end
+
+function _target_sort(fu, is_, sc, fe_)
+
+    io_ = map(!, is_)
+
+    mt_ = map(sc_ -> fu(sc_[:, io_], sc_[:, is_]), eachrow(sc))
+
+    id_ = sortperm(mt_; rev = true)
+
+    fe_[id_], mt_[id_]
+
+end
+
+"""
+Run metric-rank (standard) GSEA.
+
+# Args
+
+  - `output_directory`:
+  - `target_x_sample_x_number_tsv`:
+  - `feature_x_sample_x_score_tsv`:
+  - `set_features_json`:
+
+# Options
+
+  - `--normalization-dimension`: = 0. 0 (not normalizing) | 1 | 2.
+  - `--normalization-standard-deviation`: = 4.
+  - `--algorithm`: = "ks". "ks" | "ksa" | "kliom" | "kliop" | "kli" | "kli1".
+  - `--exponent`: = 1.0.
+  - `--minimum-set-size`: = 15.
+  - `--maximum-set-size`: = 500.
+  - `--minimum-set-fraction`: = 0.0.
+  - `--metric`: = "signal-to-noise-ratio". "mean-difference" | "signal-to-noise-ratio".
+  - `--permutation`: = "sample". "sample" | "set" | feature_x_index_x_random.tsv.
+  - `--number-of-permutations`: = 100.
+  - `--random-seed`: = 20150603.
+  - `--number-of-sets-to-plot`: = 4.
+  - `--more-sets-to-plot`: = "". Space-separated set names.
+  - `--feature-name`: = "Gene".
+  - `--score-name`: = "Signal-to-Noise Ratio".
+  - `--low-text`: = "Low".
+  - `--high-text`: = "High".
+
+# Flags
+
+  - `--write-set-x-index-x-random-tsv`: = false.
+"""
+@cast function metric_rank(
+    output_directory,
+    target_x_sample_x_number_tsv,
+    feature_x_sample_x_score_tsv,
+    set_features_json;
+    normalization_dimension::Int = 0,
+    normalization_standard_deviation::Float64 = 4.0,
+    algorithm = "ks",
+    exponent::Float64 = 1.0,
+    metric = "signal-to-noise-ratio",
+    minimum_set_size::Int = 15,
+    maximum_set_size::Int = 500,
+    minimum_set_fraction::Float64 = 0.0,
+    permutation = "sample",
+    number_of_permutations::Int = 100,
+    random_seed::Int = 20150603,
+    write_set_x_index_x_random_tsv::Bool = false,
+    number_of_sets_to_plot::Int = 4,
+    more_sets_to_plot = "",
+    feature_name = "Gene",
+    score_name = "Signal-to-Noise Ratio",
+    low_text = "Low",
+    high_text = "High",
+)
+
+    tt = Omics.Table.rea(target_x_sample_x_number_tsv)
+
+    tf = Omics.Table.rea(feature_x_sample_x_score_tsv)
+
+    sc = Matrix(tf[!, indexin(names(tt), names(tf))])
+
+    if !iszero(normalization_dimension)
+
+        _standardize_clamp!(sc, normalization_dimension, normalization_standard_deviation)
+
+    end
+
+    fu = if metric == "mean-difference"
+
+        _get_mean_difference
+
+    elseif metric == "signal-to-noise-ratio"
+
+        _get_signal_to_noise_ratio
+
+    end
+
+    is_ = convert(BitVector, tt[1, 2:end])
+
+    fe_, mt_ = _target_sort(fu, is_, sc, fe_)
+
+    Omics.Table.writ(
+        joinpath(output_directory, "feature_x_metric_x_score.tsv"),
+        "Feature",
+        fe_,
+        metric,
+        reshape(mt_, :, 1),
+    )
+
+    se_, me___ = _read_set(
+        set_features_json,
+        fe_,
+        minimum_set_size,
+        maximum_set_size,
+        minimum_set_fraction,
+    )
+
+    al = _set_algorithm(algorithm)
+
+    if permutation == "sample"
+
+        ra = Matrix{Float64}(undef, lastindex(se_), number_of_permutations)
+
+        if 0 < number_of_permutations
+
+            seed!(random_seed)
+
+            @showprogress for id in 1:number_of_permutations
+
+                ra[:, id] = enrich(
+                    al,
+                    _target_sort(fu, shuffle!(is_), sc, fe_)...,
+                    me___;
+                    ex = exponent,
+                )
+
+            end
+
+        end
+
+    elseif permutation == "set"
+
+        ra =
+            _permute_set(number_of_permutations, random_seed, al, fe_, sc_, me___, exponent)
+
+    elseif isfile(permutation)
+
+        ra = _use_permutation(permutation, al, fe_, me___, exponent)
+
+    end
+
+    _write(
+        output_directory,
+        write_set_x_index_x_random_tsv,
+        al,
+        fe_,
+        sc_,
+        exponent,
+        se_,
+        me___,
+        enrich(al, fe_, sc_, me___; ex = exponent),
+        ra,
+        number_of_sets_to_plot,
+        split(more_sets_to_plot),
+        feature_name,
+        score_name,
+        low_text,
+        high_text,
+    )
+
+end
+
+"""
+# Gene set enrichment analysis 🏔️
+"""
+@main
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -257,3 +257,206 @@ for al in AL_
     #@btime GSEA.enrich($al, FE_, SC, ME___)
 
 end
+
+# ---- #
+
+const JS = joinpath(DA, "set_features.json")
+
+const OU = joinpath(homedir(), "Downloads")
+
+# ---- #
+
+const AL = GSEA.KS()
+
+GSEA.plot(
+    tempdir(),
+    FE_,
+    SC,
+    AL,
+    SE_,
+    ME___,
+    "Sample",
+    ["Score", "Score x 10", "Constant"],
+    GSEA.enrich(AL, FE_, SC, ME___),
+)
+
+# ---- #
+
+for (fe_, mi, ma, fr) in (
+    (String[], 33, 36, 0),
+    (unique(vcat(values(Omics.Dic.rea(JS))...)), 33, 36, 0),
+    (["SHH", "XIST"], 1, 5656, 0),
+)
+
+    if isempty(fe_)
+
+        @test Omics.Error.@is GSEA._read_set(JS, fe_, mi, ma, fr)
+
+    else
+
+        se_, me___ = GSEA._read_set(JS, fe_, mi, ma, fr)
+
+        @test lastindex(se_) === lastindex(me___) === 2
+
+    end
+
+end
+
+# ---- #
+
+for (al, re) in zip(("ks", "ksa", "kli", "kliom", "kliop"), AL_)
+
+    GSEA._set_algorithm(al) == re
+
+end
+
+# ---- #
+
+const TF = joinpath(DA, "feature_x_sample_x_number.tsv")
+
+# ---- #
+
+const OD = mkpath(joinpath(OU, "data_rank"))
+
+GSEA.data_rank(OD, TF, JS)
+
+const TD = Omics.Table.rea(joinpath(OD, "TD.tsv"))
+
+@test size(TD) === (8, 10)
+
+@test TD[!, "Set"] == [
+    "HALLMARK_ESTROGEN_RESPONSE_LATE",
+    "HALLMARK_EPITHELIAL_MESENCHYMAL_TRANSITION",
+    "HALLMARK_ESTROGEN_RESPONSE_EARLY",
+    "HALLMARK_KRAS_SIGNALING_DN",
+    "HALLMARK_IL2_STAT5_SIGNALING",
+    "HALLMARK_APICAL_JUNCTION",
+    "HALLMARK_HYPOXIA",
+    "HALLMARK_GLYCOLYSIS",
+]
+
+# ---- #
+
+# 737.209 μs (1001 allocations: 1.47 MiB)
+# 803.333 μs (1001 allocations: 1.47 MiB)
+for al in (AL_[1], AL_[end])
+
+    seed!(20231103)
+
+    en_ = randn(100)
+
+    ra = randn(100, 1000)
+
+    GSEA._normalize_enrichment!(al, en_, ra)
+
+    @btime GSEA._normalize_enrichment!($al, $en_, $ra)
+
+end
+
+# ---- #
+
+function test_statistics(ta, ur)
+
+    @test size(ta, 1) === ur
+
+    @test names(ta) == ["Set", "Enrichment", "Normalized Enrichment", "P-Value", "Q-Value"]
+
+end
+
+function test_html(ou, uh)
+
+    @test lastindex(readdir(ou; ke_ = (r"html$",))) === uh
+
+end
+
+# ---- #
+
+const OS = mkpath(joinpath(OU, "user_rank"))
+
+GSEA.user_rank(
+    OS,
+    joinpath(DA, "feature_x_metric_x_score.tsv"),
+    JS;
+    number_of_sets_to_plot = 2,
+    more_sets_to_plot = "HALLMARK_MYC_TARGETS_V1 HALLMARK_UV_RESPONSE_DN HALLMARK_UV_RESPONSE_UP ALIEN",
+)
+
+const SU = Omics.Table.read(joinpath(OS, "set_x_statistic_x_number.tsv"))
+
+test_statistics(SU, 50)
+
+for (id, r1, r2) in (
+    (1, "HALLMARK_PANCREAS_BETA_CELLS", [-0.35266, -1.36616, 0.0200837]),
+    (2, "HALLMARK_PROTEIN_SECRETION", [-0.272096, -1.25207, 0.0686192]),
+    (49, "HALLMARK_MYC_TARGETS_V1", [0.603356, 2.73998, 0.000262812]),
+    (50, "HALLMARK_MYC_TARGETS_V2", [0.866579, 3.36557, 0.000262812]),
+)
+
+    @test SU[id, 1] === r1
+
+    @test isapprox(collect(SU[id, 2:4]), r2; atol = 0.00001)
+
+end
+
+test_html(OS, 6)
+
+# ---- #
+
+const ZE_ = zeros(Int, 3)
+
+@test isnan(GSEA._get_signal_to_noise_ratio(ZE_, ZE_))
+
+# 37.088 ns (0 allocations: 0 bytes)
+# 21.439 ns (0 allocations: 0 bytes)
+# 38.807 ns (0 allocations: 0 bytes)
+# 39.396 ns (0 allocations: 0 bytes)
+for (n1_, n2_, re) in (
+    (fill(1, 3), fill(0.001, 3), 4.990009990009989),
+    (collect(1:3), collect(10:10:30), -1.6363636363636365),
+    (fill(0.001, 3), fill(1, 3), -4.990009990009989),
+    (fill(0.001, 3), fill(10, 3), -4.999000099990002),
+)
+
+    @test GSEA._get_signal_to_noise_ratio(n1_, n2_) === re
+
+    @btime GSEA._get_signal_to_noise_ratio($n1_, $n2_)
+
+end
+
+# ---- #
+
+const TT = joinpath(DA, "target_x_sample_x_number.tsv")
+
+# ---- #
+
+const OM = mkpath(joinpath(OU, "metric_rank"))
+
+GSEA.metric_rank(OM, TT, TF, JS)
+
+const TM = Omics.Table.read(joinpath(OM, "feature_x_metric_x_score.tsv"))
+
+@test size(TM) === (1000, 2)
+
+@test names(TM) == ["Feature", "signal-to-noise-ratio"]
+
+@test isapprox(view(TM, [1, 1000], 2), [1.83724, -1.7411]; atol = 0.00001)
+
+test_statistics(Omics.Table.read(joinpath(OM, "set_x_statistic_x_number.tsv")), 8)
+
+test_html(OM, 8)
+
+# ---- #
+
+const TR = mkpath(joinpath(OU, "metric_rank_small"))
+
+GSEA.metric_rank(
+    TR,
+    TT,
+    TF,
+    joinpath(DA, "2set_features.json");
+    minimum_set_size = 3,
+    maximum_set_size = 3,
+    number_of_sets_to_plot = 2,
+)
+
+test_html(TR, 2)
